73b59d337702af1e0beb180d23f6ffcd05975269
NazTechNation <mac-hd@NazTech-Nation.local>
2024-07-07 14:39:56 +0100

    integrating api

--- lib/widgets/loading_screen.dart
+++ lib/widgets/loading_screen.dart
@@ -1,164 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'dart:math' as math;
 import 'package:healthbubba/res/app_colors.dart';
 
-class LoadersPage extends StatefulWidget {
+class LoadersPage extends StatelessWidget {
   final int length;
   const LoadersPage({this.length = 5, Key? key}) : super(key: key);
 
   @override
-  State<LoadersPage> createState() => _LoadersPageState();
-}
-
-class _LoadersPageState extends State<LoadersPage> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: SizedBox(
-              width: 60,
-              height: 60,
-              child: CustomCircularProgressIndicator(
-                progressColor: AppColors.lightSecondary,
-                backgroundColor: AppColors.lightSecondary.withOpacity(0.5),
-                strokeWidth: 6,
-                size: 40,
+      backgroundColor: Colors.black.withOpacity(0.5),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircularProgressIndicator(
+              valueColor: const AlwaysStoppedAnimation<Color>(AppColors.lightSecondary),
+              backgroundColor: AppColors.lightSecondary.withOpacity(0.5),
+              strokeWidth: 6,
+            ),
+            const SizedBox(height: 9),
+            Text(
+              'Please wait...',
+              style: GoogleFonts.getFont(
+                'Inter',
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w600,
+                fontSize: 14,
+                height: 1.4,
+                color: const Color(0xFF131316),
               ),
             ),
-          ),
-          const SizedBox(
-            height: 9,
-          ),
-          Text(
-            'Please wait...',
-            style: GoogleFonts.getFont(
-              fontStyle: FontStyle.italic,
-              'Inter',
-              fontWeight: FontWeight.w600,
-              fontSize: 14,
-              height: 1.4,
-              color: const Color(0xFF131316),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
-  }
-}
-
-class CustomCircularProgressIndicator extends StatefulWidget {
-  final Color progressColor;
-  final Color backgroundColor;
-  final double strokeWidth;
-  final double size;
-
-  const CustomCircularProgressIndicator({
-    super.key,
-    required this.progressColor,
-    required this.backgroundColor,
-    required this.strokeWidth,
-    required this.size,
-  });
-
-  @override
-  _CustomCircularProgressIndicatorState createState() =>
-      _CustomCircularProgressIndicatorState();
-}
-
-class _CustomCircularProgressIndicatorState
-    extends State<CustomCircularProgressIndicator>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller =
-        AnimationController(vsync: this, duration: const Duration(seconds: 1))
-          ..repeat();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: widget.size,
-      height: widget.size,
-      child: AnimatedBuilder(
-        animation: _controller,
-        builder: (context, child) {
-          return CustomPaint(
-            painter: _CircularProgressPainter(
-              progress: _controller.value,
-              progressColor: widget.progressColor,
-              backgroundColor: widget.backgroundColor,
-              strokeWidth: widget.strokeWidth,
-            ),
-          );
-        },
-      ),
-    );
-  }
-}
-
-class _CircularProgressPainter extends CustomPainter {
-  final double progress;
-  final Color progressColor;
-  final Color backgroundColor;
-  final double strokeWidth;
-
-  _CircularProgressPainter({
-    required this.progress,
-    required this.progressColor,
-    required this.backgroundColor,
-    required this.strokeWidth,
-  });
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final Paint backgroundPaint = Paint()
-      ..color = backgroundColor
-      ..strokeWidth = strokeWidth
-      ..style = PaintingStyle.stroke;
-
-    final Paint progressPaint = Paint()
-      ..color = progressColor
-      ..strokeWidth = strokeWidth
-      ..style = PaintingStyle.stroke
-      ..strokeCap = StrokeCap.round;
-
-    final double radius = (size.width / 2) - (strokeWidth / 2);
-    final Offset center = Offset(size.width / 2, size.height / 2);
-    final double startAngle = -math.pi / 2;
-    final double sweepAngle = 2 * math.pi * progress;
-
-    // Draw the background circle
-    canvas.drawCircle(center, radius, backgroundPaint);
-
-    // Draw the progress arc
-    canvas.drawArc(
-      Rect.fromCircle(center: center, radius: radius),
-      startAngle,
-      sweepAngle,
-      false,
-      progressPaint,
-    );
-  }
-
-  @override
-  bool shouldRepaint(_CircularProgressPainter oldDelegate) {
-    return oldDelegate.progress != progress ||
-        oldDelegate.progressColor != progressColor ||
-        oldDelegate.backgroundColor != backgroundColor ||
-        oldDelegate.strokeWidth != strokeWidth;
   }
 }
